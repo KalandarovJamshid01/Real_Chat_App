8a739c3374d280b330c46d87332924cfa9046ac9
Jamshid <kalandarovjamshid01@gmail.com>
2023-01-25 14:38:52 +0500

    chat online iw ready

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import "./message.css";
 
-export default function Message() {
+export default function Message({ own }) {
   return (
-    <div className="message">
+    <div className={own ? "message own" : "message"}>
       <div className="messageTop">
         <img
           src="./../../../public/assets/person/1.jpeg"
           alt=""
           className="messageImg"
         />
-        <p className="messageText">Hello this is a message</p>
+        <p className="messageText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur,
+        </p>
       </div>
       <div className="messageBottom">1 hour ago</div>
     </div>
--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -2,6 +2,7 @@ import "./messenger.css";
 import Topbar from "./../../components/topbar/Topbar";
 import Conversations from "../../components/conversations/Conversations.jsx";
 import Message from "../../components/Message/Message";
+import ChatOnline from "../../components/chatOnline/ChatOnline";
 export default function Messenger() {
   return (
     <>
@@ -25,14 +26,32 @@ export default function Messenger() {
           <div className="chatBoxWrapper">
             <div className="chatBoxTop">
               <Message />
+              <Message own={true} />
               <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
               <Message />
             </div>
-            <div className="chatBoxBottom"></div>
+            <div className="chatBoxBottom">
+              <textarea
+                className="chatMessageInput"
+                placeholder="Write something..."
+              ></textarea>
+              <button className="chatSubmitButton">Send</button>
+            </div>
           </div>
         </div>
         <div className="chatOnline">
-          <div className="chatOnlineWrapper"></div>
+          <div className="chatOnlineWrapper">
+            <ChatOnline />
+          </div>
         </div>
       </div>
     </>
